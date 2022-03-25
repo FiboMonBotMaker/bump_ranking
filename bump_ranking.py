4dc2f339b3328ce2aea86c6f036b9a047e085cd7
nikawamikan <wkwktktkkrkr@outlook.jp>
2022-03-26 03:46:51 +0900

    どうしようもない機能の追加

--- bump_ranking.py
+++ bump_ranking.py
@@ -1,12 +1,17 @@
+from copy import copy
+from googletrans import Translator
 from datetime import datetime
+import json
 import os
+import random
 import discord
+from discord import Option, OptionChoice
 from discord.ext import commands
 from dotenv import load_dotenv
 import re
 import bump_guild
 from bump_guild import ja_time
-
+import urllib.parse
 # おまじない
 load_dotenv()
 TOKEN = os.getenv('NTOKEN')
@@ -138,7 +143,7 @@ async def send_csv(ctx):
     for bumper in bumper_guilds[ctx.guild.id].get_bumper():
         csv.write(f'{bumper[0]},{bumper[1]},{bumper[2]}\n')
     csv.close()
-    await ctx.respond(file=discord.File('bumpdate.csv'))
+    await ctx.interaction.edit_original_message(content='', file=discord.File('bumpdate.csv'))
 
 
 # デフォルト系コマンドを定義
@@ -205,11 +210,116 @@ async def set_bump_channel(ctx):
     await ctx.respond(f'Bumpチャンネルを{ctx.channel.name}にセットしたよ')
 
 
+@bot.slash_command(description='褒められたときに名前を入れて使いましょう', guild_ids=guild_ids)
+async def get_home(
+        ctx,
+        name: Option(str, required=False, description='ない場合はNBさんになります'),
+        homenai: Option(str, required=False, description='もし、褒めないられない場合はここで変更'),
+        nanika: Option(str, required=False, description='もし、自信以外の場合はここで変更')):
+    if not name:
+        name = 'NB'
+    if not homenai:
+        homenai = "褒められる"
+    if not nanika:
+        nanika = '自信'
+
+    up = f'{name}さんに{homenai}と'
+    down = f'{nanika}になります！'
+
+    await ctx.respond(f'https://gsapi.cyberrex.jp/image?top={urllib.parse.quote(up)}&bottom={urllib.parse.quote(down)}')
+
+
+values = [
+    OptionChoice(name='悲報', value='【悲報wwwwwwwwww】'),
+    OptionChoice(name='朗報', value='【朗報wwwwwwwwww】'),
+    OptionChoice(name='良報', value='【良報WWwwwWwWwWww】')
+]
+
+
+@bot.slash_command(description='NB構文Y型', guild_ids=guild_ids)
+async def get_nb2(
+    ctx,
+    any_hou: Option(str, choices=values, required=False, description='何報ですか？'),
+    honbun: Option(str, required=False,
+                   description='本文を入力しよう→{本文}しまうwwwwwwwwww')):
+    if not any_hou:
+        any_hou = values[0].value
+    if not honbun:
+        honbun = 'ワイ氏パチスロにいって'
+    down = f'{honbun}しまうwwwwwwwwww'
+    await ctx.respond(f'https://gsapi.cyberrex.jp/image?top={urllib.parse.quote(any_hou)}&bottom={urllib.parse.quote(down)}')
+
+itudokocommand = setcommand = bot.create_group(
+    name="itudoko", description="いつどこします", guild_ids=guild_ids)
+
+itudoko = [
+    OptionChoice(name='いつ', value=0),
+    OptionChoice(name='どこで', value=1),
+    OptionChoice(name='だれが', value=2),
+    OptionChoice(name='何をした', value=3)
+]
+
+FILE_NAME = 'itudoko.json'
+
+with open(FILE_NAME, mode='rt', encoding='utf-8') as file:
+    stack: list[list[str]] = json.load(file)
+
+
+def wright_json(stack: list[list[str]]):
+    with open(FILE_NAME, mode='wt', encoding='utf-8') as file:
+        json.dump(stack, file, ensure_ascii=False)
+
+
+@itudokocommand.command(name="set", description="いつどこワードを追加します")
+async def itudokoset(ctx, choise: Option(int, choices=itudoko), value: Option(str, description='リテラルを決定してね')):
+    stack[choise].append(value)
+    wright_json(stack=stack)
+    await ctx.respond(f'{value}をセットしました', ephemeral=True)
+
+
+@itudokocommand.command(name='get', description='今まで貯めた文字列でランダムにいつどこいます')
+async def itudokoget(ctx):
+    await ctx.respond(
+        f'{random.choice(stack[0])}{random.choice(stack[1])}{random.choice(stack[2])}{random.choice(stack[3])}')
+
+
+tr = Translator()
+
+lang_codes: list[str] = ['en', 'it', 'ne', 'ko', 'de']
+
+
+def random_transe(word: str, lang: str, loop: int, lang_codes: list[str]) -> str:
+    if loop == 0:
+        return tr.translate(word, src=lang, dest='ja').text
+    else:
+        random.shuffle(lang_codes)
+        tmp_lang = lang_codes.pop()
+        return random_transe(
+            word=tr.translate(word, src=lang, dest=tmp_lang).text,
+            lang=tmp_lang,
+            loop=loop - 1,
+            lang_codes=lang_codes)
+
+
+@ itudokocommand.command(name='trans', description='再翻訳で支離滅裂な文章に変換します')
+async def itudokotrans(ctx, loop: Option(int, description='再翻訳回数を上げて精度を低めます デフォルト loop=1', min_value=1, max_value=5, default=1, required=False)):
+    await ctx.respond('wait')
+    await ctx.interaction.edit_original_message(
+        content=random_transe(
+            word=f'{random.choice(stack[0])}{random.choice(stack[1])}{random.choice(stack[2])}{random.choice(stack[3])}',
+            lang='ja',
+            loop=loop,
+            lang_codes=copy(lang_codes)
+        )
+    )
+
+
 @bot.event
 async def on_application_command_error(ctx, error):
     if isinstance(error, commands.CommandOnCooldown):
         await ctx.respond(error, ephemeral=True)
     else:
         raise error
+
 
 bot.run(TOKEN)
